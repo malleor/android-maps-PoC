b8a2a75a19eb19230f072377b9233e47700d01ae
Janusz Lenar <malleor@gmail.com>
2014-11-18 21:56:15 +0100

    Updating the user's position on the map

--- app/src/main/java/org/malleor/mapspoc/MapsActivity.java
+++ app/src/main/java/org/malleor/mapspoc/MapsActivity.java
@@ -1,28 +1,98 @@
 package org.malleor.mapspoc;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final String TAG = "***************";
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private Marker mMyMarker = null;
+
+    LocationClient mLocationClient;
+    LocationRequest mLocationRequest;
+    private static final long UPDATE_INTERVAL = 2000;
+    private static final long FASTEST_INTERVAL = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d(TAG, "Google Play services is available.");
+        } else {
+            Log.e(TAG, String.format("Google Play services is unavailable, 'cause %d", resultCode));
+        }
+
+        mLocationClient = new LocationClient(this, this, this);
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mLocationClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // If the client is connected
+        if (mLocationClient.isConnected()) {
+            /*
+             * Remove location updates for a listener.
+             * The current Activity is the listener, so
+             * the argument is "this".
+             */
+            mLocationClient.removeLocationUpdates(this);
+        }
+        /*
+         * After disconnect() is called, the client is
+         * considered "dead".
+         */
+        mLocationClient.disconnect();
+        super.onStop();
     }
 
     /**
@@ -60,6 +130,35 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMyMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Me").alpha(0));
+    }
+
+
+
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        Log.i(TAG, "Connected to Google Play Services.");
+
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.w(TAG, "Disconnected from Google Play Services.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "Connection to Google Play Services failed!");
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, "========== LOCATION CHANGED ==========");
+        LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
+        mMyMarker.setPosition(pos);
+        mMyMarker.setAlpha(1);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 16));
     }
 }
